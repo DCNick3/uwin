e196bea3fe668289cf2991bea6e985620b3c2dd6
Nikita Strygin <nikita6@bk.ru>
2021-12-02 16:35:56 +0300

    Fiddle with optimization pipeline (it's broken =()

--- recompilation/lifter/src/Optimize.cpp
+++ recompilation/lifter/src/Optimize.cpp
@@ -1,10 +1,9 @@
 
 #include "Optimize.h"
+#include "remill/BC/Util.h"
 
 #include <llvm/IR/Dominators.h>
 #include <llvm/IR/Verifier.h>
-#include <llvm/Transforms/IPO.h>
-#include <llvm/Transforms/IPO/AlwaysInliner.h>
 #include <llvm/Analysis/ProfileSummaryInfo.h>
 #include <llvm/Analysis/BranchProbabilityInfo.h>
 #include <llvm/Analysis/LoopInfo.h>
@@ -13,11 +12,24 @@
 #include <llvm/Analysis/AliasAnalysis.h>
 #include <llvm/Analysis/BlockFrequencyInfo.h>
 #include <llvm/Analysis/AssumptionCache.h>
+#include <llvm/Analysis/MemorySSA.h>
+
+#include <llvm/Transforms/Utils/Cloning.h>
+
+#include <llvm/Transforms/IPO.h>
+#include <llvm/Transforms/IPO/AlwaysInliner.h>
+
+#include <llvm/Transforms/Scalar/SROA.h>
+#include <llvm/Transforms/Scalar/EarlyCSE.h>
+#include <llvm/Transforms/Scalar/SimplifyCFG.h>
+#include <llvm/Transforms/Scalar/Reassociate.h>
+#include <llvm/Transforms/Scalar/SCCP.h>
 
 #include <glog/logging.h>
 
 namespace remill::uwin {
 
+namespace {
 // copy all lifted functions into a new module, discard the old one
 class RemillSemanticsStripperPass : public llvm::PassInfoMixin<RemillSemanticsStripperPass> {
   std::set<std::string> const& _lifted_functions;
@@ -29,71 +41,62 @@ public:
   llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &) {
     LOG(INFO) << "Running RemillSemanticsStripperPass";
 
-    std::vector<llvm::StringRef> rm_list;
+    // clone the module
+    auto M1 = llvm::CloneModule(M);
 
-    // TODO: this is a hack that might remove somthing that should not have been removed
-    M.getGlobalVariable("llvm.compiler.used")->eraseFromParent();
+    LOG(INFO) << "Cleaning module...";
+    // clean the module, same as in the destructor
+    // (Is it still safe to use it?)
+    M.dropAllReferences();
+    M.getGlobalList().clear();
+    M.getFunctionList().clear();
+    M.getAliasList().clear();
+    M.getIFuncList().clear();
+    M.getNamedMDList().clear();
 
-    // this function keeps references to all the intrinsics
-    auto intr = M.getFunction("__remill_intrinsics");
-    if (intr != nullptr) {
-      intr->replaceAllUsesWith(llvm::UndefValue::get(intr->getType()));
-      intr->eraseFromParent();
+    LOG(INFO) << "Moving all the functions into the clean module...";
+    for (auto& fun_name : _lifted_functions) {
+      auto fun = M1->getFunction(fun_name);
+      CHECK(fun != nullptr);
+      remill::MoveFunctionIntoModule(fun, &M);
     }
-
-    for (auto& gl : M.globals()) {
-      if (gl.getName().startswith("ISEL_")) {
-        rm_list.emplace_back(gl.getName());
-      }
-    }
-
-    for (auto gl_name : rm_list) {
-      auto gl = M.getGlobalVariable(gl_name);
-      gl->replaceAllUsesWith(llvm::UndefValue::get(gl->getType()));
-      gl->eraseFromParent();
-    }
-
-    /*rm_list.clear();
-
-    for (auto& fun : M.functions()) {
-      if (_lifted_functions.find(fun.getName().str()) == _lifted_functions.end())
-        rm_list.emplace_back(fun.getName());
-      //else
-      //  LOG(INFO) << "Not removing " << fun.getName().str();
-    }
-
-    for (auto const& fun_name : rm_list) {
-      auto fun = M.getFunction(fun_name);
-      bool droppable = true;
-      for (auto const& user : fun->users()) {
-        //user->dump();
-        droppable &= user->isDroppable();
-      }
-      if (droppable) {
-        fun->eraseFromParent();
-      }
-      else {
-        if (!fun->isDeclaration()) {
-          LOG(WARNING) << "Can't remove " << fun->getName().str() << ", as it has some uses";
-        }
-        //fun->eraseFromParent();
-      }
-    }*/
 
     return {}; // invalidate everything (because doing otherwise is hard)
   }
   static bool isRequired() { return true; }
 };
 
+auto BuildFunctionSimplificationPipeline() {
+  llvm::FunctionPassManager FPM;
+
+  // Kind of based on -O1 pipeline, but I have no idea of what I am doing
+  FPM.addPass(llvm::SROA()); // scalar replacement of aggregates (wut?)
+  FPM.addPass(llvm::EarlyCSEPass(true)); // common subexpression elimination
+
+  // invokePeepholeEPCallbacks ???
+
+  FPM.addPass(llvm::SimplifyCFGPass());
+  FPM.addPass(llvm::ReassociatePass()); // is it useful?
+
+  // no loops here, as there are no loops yet, all instrs live in their own world
+
+  FPM.addPass(llvm::SCCPPass()); // constant propagation and merging
+
+  return FPM;
+}
+
+}
+
 void OptimizeUwinModule(llvm::Module &module, std::set<std::string> const& lifted_functions) {
   llvm::ModuleAnalysisManager MAM;
   llvm::FunctionAnalysisManager FAM;
 
-  MAM.registerPass([&](){return llvm::PassInstrumentationAnalysis();});
   MAM.registerPass([&](){return llvm::InnerAnalysisManagerProxy<llvm::FunctionAnalysisManager, llvm::Module>(FAM);});
+  MAM.registerPass([&](){return llvm::PassInstrumentationAnalysis();});
   MAM.registerPass([&](){return llvm::ProfileSummaryAnalysis();});
   MAM.registerPass([&](){return llvm::VerifierAnalysis();});
 
+  FAM.registerPass([&](){return llvm::OuterAnalysisManagerProxy<llvm::ModuleAnalysisManager, llvm::Function>(MAM);});
   FAM.registerPass([&](){return llvm::PassInstrumentationAnalysis();});
   FAM.registerPass([&](){return llvm::AssumptionAnalysis();});
   FAM.registerPass([&](){return llvm::BlockFrequencyAnalysis();});
@@ -103,13 +106,20 @@ void OptimizeUwinModule(llvm::Module &module, std::set<std::string> const& lifte
   FAM.registerPass([&](){return llvm::PostDominatorTreeAnalysis();});
   FAM.registerPass([&](){return llvm::TargetLibraryAnalysis();});
   FAM.registerPass([&](){return llvm::AAManager();});
+  FAM.registerPass([&](){return llvm::TargetIRAnalysis();});
+  FAM.registerPass([&](){return llvm::MemorySSAAnalysis();});
 
   llvm::ModulePassManager MPM;
 
   // inline all intrinsics and semantics
   // don't want lifetime information as not to bloat the bitcode
   MPM.addPass(llvm::AlwaysInlinerPass(false));
-  MPM.addPass(RemillSemanticsStripperPass(lifted_functions));
+  
+  // TODO: focus only on lifted functions
+  MPM.addPass(llvm::createModuleToFunctionPassAdaptor(
+    BuildFunctionSimplificationPipeline()
+  ));
+  //MPM.addPass(RemillSemanticsStripperPass(lifted_functions));
   MPM.addPass(llvm::VerifierPass());
 
   MPM.run(module, MAM);
