dd0e572639252c55704730fe738e62db748994b1
Nikita Strygin <nikita6@bk.ru>
2022-04-05 18:30:22 +0300

    Move load_process_image into loader module

--- recompiler/src/loader.rs
+++ recompiler/src/loader.rs
@@ -1,10 +1,14 @@
+use itertools::Itertools;
+use lazy_static::lazy_static;
 use memory_image::{MemoryImage, Protection};
 use num::Integer;
 use object::read::pe::ImageNtHeaders;
 use object::{pe, LittleEndian, Object};
 use std::cmp::max;
+use std::collections::{BTreeMap, HashSet};
 
 use crate::error::Result;
+use crate::make_dll_stub;
 use crate::pe_file::{LoadedPeInfo, PeFile};
 
 pub const LE: LittleEndian = LittleEndian {};
@@ -99,4 +103,95 @@ impl PeFile {
 
         Ok(LoadedPeInfo::new(addr, max_rva))
     }
+}
+
+// TODO: populate this list
+lazy_static! {
+    static ref STUBBUABLE_DLLS: HashSet<&'static str> =
+        HashSet::from(["kernel32.dll", "user32.dll"]);
+}
+
+pub fn load_process_image(executable: PeFile, dlls: Vec<PeFile>) -> Result<MemoryImage> {
+    let mut dlls = dlls
+        .into_iter()
+        .map(|pe| (pe.name().to_string(), pe))
+        .collect::<BTreeMap<_, _>>();
+
+    let mut required_dlls = BTreeMap::new();
+
+    for (dll, group) in &dlls
+        .values()
+        .chain([&executable])
+        .flat_map(|f| f.get().imports().unwrap())
+        .map(|import| {
+            (
+                std::str::from_utf8(import.library())
+                    .expect("Library with non-ascii name")
+                    .to_ascii_lowercase(),
+                std::str::from_utf8(import.name()).expect("Imported function with non-ascii name"),
+            )
+        })
+        .sorted_by_key(|(dll, _)| dll.clone()) // clone here is just me vs the borrow checker
+        .group_by(|(dll, _)| dll.clone())
+    {
+        let functions = group
+            .map(|(_, name)| name.to_string())
+            .unique()
+            .collect::<Vec<_>>();
+        required_dlls.insert(dll, functions);
+    }
+
+    let fn_indices = required_dlls
+        .values()
+        .flatten()
+        .unique()
+        .enumerate()
+        .map(|(i, name)| (name.to_string(), i as u32))
+        .collect::<BTreeMap<_, _>>();
+
+    println!("All dep dlls collected: {:#?}", required_dlls);
+
+    for (dll_name, fns) in required_dlls.iter() {
+        if dlls.contains_key(dll_name.as_str()) {
+            println!("FOUND {}", dll_name)
+        } else if STUBBUABLE_DLLS.contains(dll_name.as_str()) {
+            println!("STUB  {}", dll_name);
+            let fns = fns
+                .iter()
+                .map(|name| (name.to_string(), *fn_indices.get(name).unwrap()))
+                .collect();
+
+            let stub = make_dll_stub(dll_name, &fns).unwrap();
+            dlls.insert(dll_name.clone(), stub);
+        } else {
+            println!("WHERE {}", dll_name);
+            panic!("Can't find dll with name {}. It was not provided as an input & is not included in stubbable allow-list", dll_name)
+        }
+    }
+
+    // start from the base image of the executable.
+    // the executable will be loaded at the requested address this way, as we load it first
+    let mut free_addr = executable.base_addr();
+
+    let mut memory = MemoryImage::new();
+
+    let mut load_into_first_free = |pe: &PeFile| -> Result<_> {
+        println!("LOAD {}", pe.name());
+        // TODO: store it somewhere
+        let info = pe.load_into(free_addr, &mut memory, pe.name())?;
+
+        free_addr += info.image_size();
+
+        Ok(())
+    };
+
+    load_into_first_free(&executable)?;
+
+    for (dll, _) in required_dlls.iter() {
+        if let Some(dll) = dlls.get(dll) {
+            load_into_first_free(dll)?
+        }
+    }
+
+    Ok(memory)
 }
--- recompiler/src/main.rs
+++ recompiler/src/main.rs
@@ -1,14 +1,10 @@
 extern crate core;
 
 use clap::Parser;
-use itertools::Itertools;
-use lazy_static::lazy_static;
-use memory_image::MemoryImage;
-use object::{LittleEndian, Object};
-use recompiler::make_dll_stub;
+use object::LittleEndian;
+use recompiler::load_process_image;
 
 use recompiler::PeFile;
-use std::collections::{BTreeMap, HashSet};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -25,11 +21,6 @@ struct Args {
 
 pub const LE: LittleEndian = LittleEndian {};
 
-lazy_static! {
-    static ref STUBBUABLE_DLLS: HashSet<&'static str> =
-        HashSet::from(["kernel32.dll", "user32.dll"]);
-}
-
 fn main() {
     let args = Args::parse();
 
@@ -42,84 +33,7 @@ fn main() {
         .collect::<Result<Vec<_>, _>>()
         .expect("Loading dlls");
 
-    let mut dlls = dlls
-        .into_iter()
-        .map(|pe| (pe.name().to_string(), pe))
-        .collect::<BTreeMap<_, _>>();
-
-    let mut required_dlls = BTreeMap::new();
-
-    for (dll, group) in &dlls
-        .values()
-        .chain([&exe])
-        .flat_map(|f| f.get().imports().unwrap())
-        .map(|import| {
-            (
-                std::str::from_utf8(import.library())
-                    .expect("Library with non-ascii name")
-                    .to_ascii_lowercase(),
-                std::str::from_utf8(import.name()).expect("Imported function with non-ascii name"),
-            )
-        })
-        .sorted_by_key(|(dll, _)| dll.clone()) // clone here is just me vs the borrow checker
-        .group_by(|(dll, _)| dll.clone())
-    {
-        let functions = group
-            .map(|(_, name)| name.to_string())
-            .unique()
-            .collect::<Vec<_>>();
-        required_dlls.insert(dll, functions);
-    }
-
-    let fn_indices = required_dlls
-        .values()
-        .flatten()
-        .unique()
-        .enumerate()
-        .map(|(i, name)| (name.to_string(), i as u32))
-        .collect::<BTreeMap<_, _>>();
-
-    println!("All dep dlls collected: {:#?}", required_dlls);
-
-    for (dll_name, fns) in required_dlls.iter() {
-        if dlls.contains_key(dll_name.as_str()) {
-            println!("FOUND {}", dll_name)
-        } else if STUBBUABLE_DLLS.contains(dll_name.as_str()) {
-            println!("STUB  {}", dll_name);
-            let fns = fns
-                .iter()
-                .map(|name| (name.to_string(), *fn_indices.get(name).unwrap()))
-                .collect();
-
-            let stub = make_dll_stub(dll_name, &fns).unwrap();
-            dlls.insert(dll_name.clone(), stub);
-        } else {
-            println!("WHERE {}", dll_name);
-            panic!("Can't find dll with name {}. It was not provided as an input & is not included in stubbable allow-list", dll_name)
-        }
-    }
-
-    let mut free_addr = exe.base_addr();
-
-    let mut memory = MemoryImage::new();
-
-    let mut load_free = |pe: &PeFile| {
-        println!("LOAD {}", pe.name());
-        // TODO: store it somewhere
-        let info = pe
-            .load_into(free_addr, &mut memory, pe.name())
-            .expect("Loading a PE file");
-
-        free_addr += info.image_size();
-    };
-
-    load_free(&exe);
-
-    for (dll, _) in required_dlls.iter() {
-        if let Some(dll) = dlls.get(dll) {
-            load_free(dll)
-        }
-    }
+    let memory = load_process_image(exe, dlls).expect("Loading process image");
 
     println!("{}", memory.map());
 }
