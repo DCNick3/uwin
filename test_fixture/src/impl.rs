83adf19668ca2c1055d1551384aef89713e2c1cf
Nikita Strygin <nikita6@bk.ru>
2022-04-22 19:51:32 +0300

    Implement a stub GetStartupInfoA (doesn't work yet though due to missing impls of FromIntoMemory for structs)

--- test_fixture/src/impl.rs
+++ test_fixture/src/impl.rs
@@ -10,6 +10,7 @@ use win32::Win32::Foundation::{HINSTANCE, HWND};
 use win32::Win32::System::Memory::{
     HeapHandle, HEAP_FLAGS, HEAP_NO_SERIALIZE, PAGE_PROTECTION_FLAGS, VIRTUAL_ALLOCATION_TYPE,
 };
+use win32::Win32::System::Threading::STARTUPINFOA;
 use win32::Win32::UI::WindowsAndMessaging::{MESSAGEBOX_RESULT, MESSAGEBOX_STYLE};
 use win32_heapmgr::HeapMgr;
 use win32_virtmem::VirtualMemoryManager;
@@ -125,5 +126,28 @@ pub struct Threading {}
 impl win32::Win32::System::Threading::Api for Threading {
     fn ExitProcess(&self, unwind_token: &mut UnwindToken, u_exit_code: u32) {
         unwind_token.unwind(UnwindReason::ExitProcess(u_exit_code));
+    }
+
+    fn GetStartupInfoA(&self, lp_startup_info: MutPtr<STARTUPINFOA>) {
+        lp_startup_info.write(STARTUPINFOA {
+            cb: 68,
+            lpReserved: PSTR(MutPtr::new(0)),
+            lpDesktop: PSTR(MutPtr::new(0)),
+            lpTitle: PSTR(MutPtr::new(0)),
+            dwX: 0,
+            dwY: 0,
+            dwXSize: 0,
+            dwYSize: 0,
+            dwXCountChars: 0,
+            dwYCountChars: 0,
+            dwFillAttribute: 0,
+            dwFlags: Default::default(),
+            wShowWindow: 0,
+            cbReserved2: 0,
+            lpReserved2: MutPtr::new(0),
+            hStdInput: Default::default(),
+            hStdOutput: Default::default(),
+            hStdError: Default::default(),
+        })
     }
 }
