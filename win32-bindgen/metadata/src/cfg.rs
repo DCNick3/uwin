ac6b8297eac28124f99fa2ebea010e06089f0ff8
Nikita Strygin <nikita6@bk.ru>
2022-05-23 17:37:34 +0300

    Fix windows-metadata tests

--- win32-bindgen/metadata/src/cfg.rs
+++ win32-bindgen/metadata/src/cfg.rs
@@ -9,7 +9,10 @@ pub struct Cfg {
 
 impl Cfg {
     pub fn new() -> Self {
-        Self { types: BTreeMap::new(), arches: BTreeSet::new() }
+        Self {
+            types: BTreeMap::new(),
+            arches: BTreeSet::new(),
+        }
     }
 
     pub fn features(&self, namespace: &str) -> Vec<&'static str> {
@@ -33,7 +36,10 @@ impl Cfg {
     }
 
     pub(crate) fn add_type(&mut self, def: &TypeDef) -> bool {
-        self.types.entry(def.namespace()).or_default().insert(def.row.clone())
+        self.types
+            .entry(def.namespace())
+            .or_default()
+            .insert(def.row.clone())
     }
 
     pub fn add_feature(&mut self, feature: &'static str) {
@@ -66,8 +72,13 @@ impl Cfg {
 
     pub fn union(&self, other: &Self) -> Self {
         let mut union = Self::new();
-        self.types.keys().for_each(|feature| union.add_feature(feature));
-        other.types.keys().for_each(|feature| union.add_feature(feature));
+        self.types
+            .keys()
+            .for_each(|feature| union.add_feature(feature));
+        other
+            .types
+            .keys()
+            .for_each(|feature| union.add_feature(feature));
         self.arches.iter().for_each(|arch| {
             union.arches.insert(arch);
         });
@@ -96,10 +107,22 @@ mod tests {
 
     #[test]
     fn test_starts_with() {
-        assert!(starts_with("Windows.Win32.Graphics.Direct3D11on12", "Windows.Win32.Graphics.Direct3D11on12"));
-        assert!(starts_with("Windows.Win32.Graphics.Direct3D11on12", "Windows.Win32.Graphics"));
-        assert!(!starts_with("Windows.Win32.Graphics.Direct3D11on12", "Windows.Win32.Graphics.Direct3D11"));
-        assert!(!starts_with("Windows.Win32.Graphics.Direct3D", "Windows.Win32.Graphics.Direct3D11"));
+        assert!(starts_with(
+            "Windows.Win32.Graphics.Direct3D11on12",
+            "Windows.Win32.Graphics.Direct3D11on12"
+        ));
+        assert!(starts_with(
+            "Windows.Win32.Graphics.Direct3D11on12",
+            "Windows.Win32.Graphics"
+        ));
+        assert!(!starts_with(
+            "Windows.Win32.Graphics.Direct3D11on12",
+            "Windows.Win32.Graphics.Direct3D11"
+        ));
+        assert!(!starts_with(
+            "Windows.Win32.Graphics.Direct3D",
+            "Windows.Win32.Graphics.Direct3D11"
+        ));
     }
 
     #[test]
@@ -109,35 +132,53 @@ mod tests {
         assert_eq!(namespaces.len(), 1);
         assert_eq!(namespaces[0], "Windows.Foundation");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Devices.Display.Core", "DisplayPresentationRate"));
+        let def = TypeReader::get()
+            .expect_type_def(("Windows.Devices.Display.Core", "DisplayPresentationRate"));
         let namespaces = def.cfg().features("Windows");
         assert_eq!(namespaces.len(), 2);
         assert_eq!(namespaces[0], "Windows.Devices.Display.Core");
         assert_eq!(namespaces[1], "Windows.Foundation.Numerics");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Graphics.DirectX.Direct3D11", "Direct3DSurfaceDescription"));
+        let def = TypeReader::get().expect_type_def((
+            "Windows.Graphics.DirectX.Direct3D11",
+            "Direct3DSurfaceDescription",
+        ));
         let namespaces = def.cfg().features("Windows");
         assert_eq!(namespaces.len(), 1);
         assert_eq!(namespaces[0], "Windows.Graphics.DirectX.Direct3D11");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Win32.Security.Authorization.UI", "EFFPERM_RESULT_LIST"));
+        let def = TypeReader::get().expect_type_def((
+            "Windows.Win32.Security.Authorization.UI",
+            "EFFPERM_RESULT_LIST",
+        ));
         let namespaces = def.cfg().features("Windows");
         assert_eq!(namespaces.len(), 2);
         assert_eq!(namespaces[0], "Windows.Win32.Foundation");
         assert_eq!(namespaces[1], "Windows.Win32.Security.Authorization.UI");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Win32.AI.MachineLearning.WinML", "MLOperatorEdgeDescription"));
+        let def = TypeReader::get().expect_type_def((
+            "Windows.Win32.AI.MachineLearning.WinML",
+            "MLOperatorEdgeDescription",
+        ));
         let namespaces = def.cfg().features("Windows");
         assert_eq!(namespaces.len(), 1);
         assert_eq!(namespaces[0], "Windows.Win32.AI.MachineLearning.WinML");
 
-        let def = TypeReader::get().get_type(("Windows.Win32.Graphics.Direct3D11on12", "D3D11On12CreateDevice")).unwrap();
+        let def = TypeReader::get()
+            .get_type((
+                "Windows.Win32.Graphics.Direct3D11on12",
+                "D3D11On12CreateDevice",
+            ))
+            .unwrap();
         let namespaces = def.cfg().features("Windows");
-        assert_eq!(namespaces.len(), 2);
-        assert_eq!(namespaces[0], "Windows.Win32.Graphics.Direct3D");
-        assert_eq!(namespaces[1], "Windows.Win32.Graphics.Direct3D11");
+        assert_eq!(namespaces.len(), 4);
+        assert_eq!(namespaces[0], "Windows.Win32.Foundation");
+        assert_eq!(namespaces[1], "Windows.Win32.Graphics.Direct3D");
+        assert_eq!(namespaces[2], "Windows.Win32.Graphics.Direct3D11");
+        assert_eq!(namespaces[3], "Windows.Win32.Graphics.Dxgi.Common");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Win32.System.Diagnostics.Debug", "CONTEXT"));
+        let def = TypeReader::get()
+            .expect_type_def(("Windows.Win32.System.Diagnostics.Debug", "CONTEXT"));
         let namespaces = def.cfg().features("Windows");
         assert_eq!(namespaces.len(), 2);
         assert_eq!(namespaces[0], "Windows.Win32.System.Diagnostics.Debug");
@@ -150,31 +191,51 @@ mod tests {
         let namespaces = def.cfg().features("Windows.Foundation");
         assert_eq!(namespaces.len(), 0);
 
-        let def = TypeReader::get().expect_type_def(("Windows.Devices.Display.Core", "DisplayPresentationRate"));
+        let def = TypeReader::get()
+            .expect_type_def(("Windows.Devices.Display.Core", "DisplayPresentationRate"));
         let namespaces = def.cfg().features("Windows.Devices.Display.Core");
         assert_eq!(namespaces.len(), 1);
         assert_eq!(namespaces[0], "Windows.Foundation.Numerics");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Graphics.DirectX.Direct3D11", "Direct3DSurfaceDescription"));
+        let def = TypeReader::get().expect_type_def((
+            "Windows.Graphics.DirectX.Direct3D11",
+            "Direct3DSurfaceDescription",
+        ));
         let namespaces = def.cfg().features("Windows.Graphics.DirectX.Direct3D11");
         assert_eq!(namespaces.len(), 0);
 
-        let def = TypeReader::get().expect_type_def(("Windows.Win32.Security.Authorization.UI", "EFFPERM_RESULT_LIST"));
-        let namespaces = def.cfg().features("Windows.Win32.Security.Authorization.UI");
+        let def = TypeReader::get().expect_type_def((
+            "Windows.Win32.Security.Authorization.UI",
+            "EFFPERM_RESULT_LIST",
+        ));
+        let namespaces = def
+            .cfg()
+            .features("Windows.Win32.Security.Authorization.UI");
         assert_eq!(namespaces.len(), 1);
         assert_eq!(namespaces[0], "Windows.Win32.Foundation");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Win32.AI.MachineLearning.WinML", "MLOperatorEdgeDescription"));
+        let def = TypeReader::get().expect_type_def((
+            "Windows.Win32.AI.MachineLearning.WinML",
+            "MLOperatorEdgeDescription",
+        ));
         let namespaces = def.cfg().features("Windows.Win32.AI.MachineLearning.WinML");
         assert_eq!(namespaces.len(), 0);
 
-        let def = TypeReader::get().get_type(("Windows.Win32.Graphics.Direct3D11on12", "D3D11On12CreateDevice")).unwrap();
+        let def = TypeReader::get()
+            .get_type((
+                "Windows.Win32.Graphics.Direct3D11on12",
+                "D3D11On12CreateDevice",
+            ))
+            .unwrap();
         let namespaces = def.cfg().features("Windows.Win32.Graphics.Direct3D11on12");
-        assert_eq!(namespaces.len(), 2);
-        assert_eq!(namespaces[0], "Windows.Win32.Graphics.Direct3D");
-        assert_eq!(namespaces[1], "Windows.Win32.Graphics.Direct3D11");
+        assert_eq!(namespaces.len(), 4);
+        assert_eq!(namespaces[0], "Windows.Win32.Foundation");
+        assert_eq!(namespaces[1], "Windows.Win32.Graphics.Direct3D");
+        assert_eq!(namespaces[2], "Windows.Win32.Graphics.Direct3D11");
+        assert_eq!(namespaces[3], "Windows.Win32.Graphics.Dxgi.Common");
 
-        let def = TypeReader::get().expect_type_def(("Windows.Win32.System.Diagnostics.Debug", "CONTEXT"));
+        let def = TypeReader::get()
+            .expect_type_def(("Windows.Win32.System.Diagnostics.Debug", "CONTEXT"));
         let namespaces = def.cfg().features("Windows.Win32.System.Diagnostics.Debug");
         assert_eq!(namespaces.len(), 1);
         assert_eq!(namespaces[0], "Windows.Win32.System.Kernel");
